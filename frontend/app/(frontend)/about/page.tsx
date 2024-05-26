8d11750d59ee18d3f90d75ed40dcdf156efce4a0
Rakib Ahsan <rakibzubayar@gmail.com>
2024-05-26 23:13:17 +0600

    T

--- frontend/app/(frontend)/about/page.tsx
+++ frontend/app/(frontend)/about/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 
 const About = () => {
-  return <div>page </div>;
+  const [test, setTest] = useState(false);
+  return (
+    <><div className="text" onClick={() => setTest(!test)}>
+      page
+      {test && <p onClick={()=>alert('ok')}>ok</p>}
+
+    </div><div>
+        <p>no css</p>
+      </div></>
+  );
 };
 
 export default About;
